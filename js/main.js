5fdbc90174dde62ec3451bdebdcb9fd2e00c33d8
Melissa Jeremie <melissajeremie@gmail.com>
2020-12-14 11:54:34 -0500

    game table

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 console.log("'tis I, the JS file");
 
-const canvas = document.getElementById('canvas');
+// Initialize game with player names
+while (!player1) {
+    const player1 = prompt("Player One - Enter name below");
+}
+let p1Color = "";
+
+while (!player2) {
+    const player2 = prompt("Player Two - Enter name below");
+}
+let p2Color = "";
+
+// Game cell selectors
+const row = document.getElementsByTagName('tr');
