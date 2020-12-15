cd39c01025f82cf920578fd14caa118e653dbf5e
Melissa Jeremie <melissajeremie@gmail.com>
2020-12-15 01:13:00 -0500

    logic refactor

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
 console.log("'tis I, the JS file");
 
-// Initialize game with player names
-while (!player1) {
-    const player1 = prompt("Player One - Enter name below");
-}
-let p1Color = "";
+// // Initialize game with player names
+// while (!player1) {
+//     const player1 = prompt("Player One - Enter name below");
+// }
+// let p1Color = "";
 
-while (!player2) {
-    const player2 = prompt("Player Two - Enter name below");
-}
-let p2Color = "";
+// while (!player2) {
+//     const player2 = prompt("Player Two - Enter name below");
+// }
+// let p2Color = "";
 
-// Game selectors
-const row = document.getElementsByTagName('tr');
-const col = document.getElementsByTagName('td');
-const playersTurn = document.querySelector('.players-turn');
-const cells = document.querySelectorAll('.cell');
-const resetGame = document.querySelector('.reset');
+// // Game selectors
+// const boardRow = document.getElementsByTagName('tr');
+// const boardCol = document.getElementsByTagName('td');
+// const playersTurn = document.querySelector('.players-turn');
+// const cells = document.querySelectorAll('.cell');
+// const resetGame = document.querySelector('.reset');
 
-let currentPlayer = 1;
-let winner;
-playersTurn.textContent = `${player1}, it's your turn`
+// let currentPlayer = 1;
+// let winner;
+// playersTurn.textContent = `${player1}, it's your turn`
 
-// store cell coordinates when clicked on
+// // store cell coordinates
 
-for (let i = 0; i < col.length; i++) {
-    col[i].addEventListener('click', (e) => {
-        console.log(`${e.target}`)
-    });
+// for (let i = 0; i < col.length; i++) {
+//     col[i].addEventListener('click', (e) => {
+//         console.log(`${e.target}`)
+//     });
     
-}
+// }
+const $board = document.getElementsByTagName('tr');
