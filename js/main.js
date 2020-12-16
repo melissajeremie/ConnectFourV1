e8eeea80b01876f8c55a368a88ffec1076e5cfbd
Melissa Jeremie <melissajeremie@gmail.com>
2020-12-15 21:31:34 -0500

    board loads with player input

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 // console.log("'tis I, the JS file");
 
-while (!playerOne){
+// while (!playerOne){
     const playerOne = prompt("Welcome, Player One. Enter your name below:");
-}
+// }
 let playerOneColor = 'purple';
-while (!playerTwo){
+// while (!playerTwo){
     const playerTwo = prompt("Welcome, Player Two. Enter your name below:");
-}
+// }
 let playerTwoColor = 'orange';
 
 let currentPlayer = 1
 let winner;
 let $myTurn = $('.players-turn');
+const $resetGame = $('.reset')
     $myTurn.textContent = `It's ${playerOne}'s turn`;
 
-
+// Create game board onload
 const $board = $('#board');
 
 function buildBoard() {
@@ -28,16 +29,19 @@ function buildBoard() {
 }
 buildBoard();
 
+// Game selectors
 const $gridRow = $('tr');
 const $gridCol = $('td');
 const $slots = $('.col')
 
-// return slot coordinates 
+// return slot coordinates when clicked
 for (i = 0; i < $gridCol.length; i++) {
     $gridCol[i].addEventListener('click', (e) => {
         console.log(i)
     })
 }
+
+// functions for gameplay
 
 function colorChange(event) {
     let column = event.target.cellIndex;
@@ -49,15 +53,41 @@ function colorChange(event) {
             if (currentPlayer === 1) {
                 row[0].style.backgroundColor = 'purple';
                 if (verticalWin() || horizontalWin() || diagonalWin() || diagonalWinTwo()) {
-                    myTurn.textContent = `${playerOne} wins! Well done.`;
-                    myTurn.style.color = playerTwoColor;
+                    $myTurn.textContent = `${playerOne} wins! Well done.`;
+                    $myTurn.style.color = playerOneColor;
+                    return alert(`${playerOne} has won the game`);
+                } else if (itsATie()) {
+                    $myTurn.textContent = "It's a tie. Play again!"
+                    return alert("It's a tie");
+                } else {
+                    $myTurn.textContent = `It's ${playerTwo}'s turn`
+                    return currentPlayer = 2;
+                }
+            } else {
+                row[0].style.backgroundColor = 'orange';
+                if (verticalWin() || horizontalWin() || diagonalWin() || diagonalWinTwo()) {
+                    $myTurn.textContent = `${playerTwo} wins! Well done.`;
+                    $myTurn.style.color = playerOneColor;
+                    return alert(`${playerTwo} has won the game`);
+                } else if (itsATie()) {
+                    $myTurn.textContent = "It's a tie. Play again!"
+                    return alert("It's a tie");
+                } else {
+                    $myTurn.textContent = `It's ${playerOne}'s turn`
+                    return currentPlayer = 1;
                 }
             }
         }
         
     }
 }
-
+Array.prototype.forEach.call($gridCol, (cell) => {
+    cell.addEventListener('click', changeColor);
+    cell.style.backgroundColor = 'white';
+})
+function colorCheck(a, b, c, d){
+    return (a === b && a ===c && a === d && a !== 'white' && a !== undefined);
+}
 function verticalWin() {
 
 }
