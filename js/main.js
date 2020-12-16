d6878bf5596635dae1a836012914e93f8d6d13e4
Melissa Jeremie <melissajeremie@gmail.com>
2020-12-15 21:00:24 -0500

    building logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 // console.log("'tis I, the JS file");
 
+while (!playerOne){
+    const playerOne = prompt("Welcome, Player One. Enter your name below:");
+}
+let playerOneColor = 'purple';
+while (!playerTwo){
+    const playerTwo = prompt("Welcome, Player Two. Enter your name below:");
+}
+let playerTwoColor = 'orange';
+
+let currentPlayer = 1
+let winner;
+let $myTurn = $('.players-turn');
+    $myTurn.textContent = `It's ${playerOne}'s turn`;
+
+
 const $board = $('#board');
-// const $gridRow = $('<tr></tr>').addClass('row');
-// const $gridCol = $('<td></td>').addClass('col');
 
 function buildBoard() {
 
@@ -13,19 +26,46 @@ function buildBoard() {
         $('.row').append('<td class = col></td>');
         } 
 }
-
 buildBoard();
-// $(document).ready(function () {
-    
-// });
 
-function horizontalWin() {
+const $gridRow = $('tr');
+const $gridCol = $('td');
+const $slots = $('.col')
 
+// return slot coordinates 
+for (i = 0; i < $gridCol.length; i++) {
+    $gridCol[i].addEventListener('click', (e) => {
+        console.log(i)
+    })
+}
+
+function colorChange(event) {
+    let column = event.target.cellIndex;
+    let row = [];
+    for (let i = 5; i > -1; i--) {
+        if ($gridRow[i].children[column].style.backgroundColor == 'white') {
+            row.push($gridRow[i].children[column]);
+
+            if (currentPlayer === 1) {
+                row[0].style.backgroundColor = 'purple';
+                if (verticalWin() || horizontalWin() || diagonalWin() || diagonalWinTwo()) {
+                    myTurn.textContent = `${playerOne} wins! Well done.`;
+                    myTurn.style.color = playerTwoColor;
+                }
+            }
+        }
+        
+    }
 }
 
 function verticalWin() {
 
 }
+
+function horizontalWin() {
+
+}
+
 
 function diagonalWin() {
 
@@ -38,3 +78,7 @@ function diagonalWinTwo() {
 function itsATie() {
 
 }
+
+// $( () => {
+    
+// });
